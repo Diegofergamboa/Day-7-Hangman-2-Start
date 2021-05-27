3076fdf1d04890f64f568a862b722f23918013b7
Diegofergamboa <>
2021-05-27 20:02:12 +0000

    Complete

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-#Step 2
-
-import random
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
-guess = input("Guess a letter: ").lower()
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 #Libreria random
 import random
 #Lista con palabras random
@@ -34,13 +8,25 @@ chosen_word = random.choice(word_list)
 display = list(chosen_word)
 print(display)
 
-''' First Challenge  
-'''
-# Inicializacion de h como contenedor
-h = ""
-# Conversion de h entre lista para separar cada elemento
-underscore = list(h) #Palabra cuasi mas importante
-#Generacion de la lista con los underscore 
-for i in display :
-    underscore.append("_") 
+word_lenght = len(display)    
+user_choice = input('Choose a letter: ') 
 
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+    
+    
+for position in range(word_lenght):
+    letter = chosen_word[position]
+    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == user_choice:
+        display[position] = letter
+
+
+
+
+for position in range(word_lenght) :
+    letter = display[position]
+    
+print(display)
